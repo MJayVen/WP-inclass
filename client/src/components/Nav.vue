ffafad87cf7c7645c9f7ffcfc655c74136b652fb
Mark Venuto <markvenutobass@gmail.com>
2022-10-13 16:14:07 -0400

    added new view + login component

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,68 +1,75 @@
 <script setup lang="ts">
-    import { ref } from 'vue';
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+import LoginBadge from './LoginBadge.vue';
 
-    let isActive = ref(false); // isActive is a reactive variable. its an object of type ref who's value is false
+let isActive = ref(false); // isActive is a reactive variable. its an object of type ref who's value is false
 </script>
 
 <template>
     <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-            <a class="navbar-item" href="https://bulma.io">
-                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-            </a>
-
-            <a :class="{ 'is-active': isActive }" @click="isActive = !isActive" role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
-        </div>
-
-        <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': isActive }" >
-            <div class="navbar-start">
-                <a class="navbar-item">
-                    Home
+        <div class="container">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="https://bulma.io">
+                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
                 </a>
 
-                <a class="navbar-item">
-                    Documentation
+                <a :class="{ 'is-active': isActive }" @click="isActive = !isActive" role="button" class="navbar-burger"
+                    aria-label="menu" aria-expanded="false">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
                 </a>
-
-                <div class="navbar-item has-dropdown is-hoverable">
-                    <a class="navbar-link">
-                        More
-                    </a>
-
-                    <div class="navbar-dropdown">
-                        <a class="navbar-item">
-                            About
-                        </a>
-                        <a class="navbar-item">
-                            Jobs
-                        </a>
-                        <a class="navbar-item">
-                            Contact
-                        </a>
-                        <hr class="navbar-divider">
-                        <a class="navbar-item">
-                            Report an issue
-                        </a>
-                    </div>
-                </div>
             </div>
 
-            <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="buttons">
-                        <a class="button is-primary">
-                            <strong>Sign up</strong>
+            <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': isActive }">
+                <div class="navbar-start">
+                    <RouterLink to="/" class="navbar-item">
+                        Home
+                    </RouterLink>
+
+                    <RouterLink to="/store" class="navbar-item">
+                        Store
+                    </RouterLink>
+
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-link">
+                            More
                         </a>
-                        <a class="button is-light">
-                            Log in
-                        </a>
+
+                        <div class="navbar-dropdown">
+                            <RouterLink to="/about" class="navbar-item">
+                                About
+                            </RouterLink>
+                            <a class="navbar-item">
+                                Jobs
+                            </a>
+                            <a class="navbar-item">
+                                Contact
+                            </a>
+                            <hr class="navbar-divider">
+                            <a class="navbar-item">
+                                Report an issue
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <LoginBadge />
                     </div>
                 </div>
             </div>
         </div>
     </nav>
 </template>
+
+<style>
+/* active navbar link */
+.router-link-active {
+    /* this is a class that is added to the active link */
+    background-color: #00d115;
+    border-bottom: #00AA00 5px solid;
+}
+</style>
