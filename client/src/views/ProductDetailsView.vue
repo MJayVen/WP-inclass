8e21c0f04939eba4dd78d0bf5de9241df9879364
Mark Venuto <markvenutobass@gmail.com>
2022-10-24 17:56:28 -0400

    search bar changes

--- client/src/views/ProductDetailsView.vue
+++ client/src/views/ProductDetailsView.vue
@@ -1,13 +1,40 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+import { getProduct } from "../stores/products";
+
+const route = useRoute();
+
+const product = ref(getProduct(+route.params.id));
 
 </script>
 
 <template>
-    <div>
-
+    <div v-if="product">
+        <div class="product-image">
+            <img :src="product.thumbnail" :alt="product.title">
+        </div>
+        <div class="product-info">
+            <h3 class="title">{{ product.title }}</h3>
+            <p>{{ product.description }}</p>
+            <p class="price">
+                <span class="currency">$</span>
+                <span class="amount">{{ product.price }}</span>
+            </p>
+        </div>
+    </div>
+    <div v-else class="section">
+        <h2 class="title">Product not found</h2>
     </div>
 </template>
 
 <style scoped>
+.section {
+    display: flex;
+    background-color: aliceblue;
+}
 
+product-info {
+    padding: 1em;
+}
 </style>
