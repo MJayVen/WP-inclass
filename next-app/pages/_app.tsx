9e454eafad4266390d183447dba1d427b7e0cee0
Mark Venuto <markvenutobass@gmail.com>
2022-12-08 15:16:10 -0500

    converting vue app into next react app

---
+++ next-app/pages/_app.tsx
@@ -0,0 +1,17 @@
+import "bulma";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Nav from "../components/Nav";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className="root">
+      <header>
+        <Nav />
+      </header>
+      <div className="container">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
+}
